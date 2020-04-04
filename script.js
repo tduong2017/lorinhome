5ab5d1791b19b6b5f2166dc2c3c1b9aac4f60518
tduong2017 <tonyndu@outlook.com>
2020-04-04 07:06:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var span = document.getElementById('time');
 
 function time() {
-  var d = new Date();
-  var s = d.getSeconds();
-  var m = d.getMinutes();
-  var h = d.getHours();
-  if (s < 10) {
-	s = "0" + s;
-  }
-  if (m < 10) {
-	m = "0" + m;
-  }
-  if (h < 10) {
-	h = "0" + h;
-  }
-  span.textContent = h + ":" + m + ":" + s;
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    var ampm = h >= 12 ? "PM" : "AM";
+    h = h % 12 || 12;
+    if (s < 10) {
+        s = "0" + s;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    span.textContent = h + ":" + m + ":" + s + " " + ampm;
 }
 
+time();
 setInterval(time, 1000);
